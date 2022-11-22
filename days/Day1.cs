1a78fde4c88083db0893a300591deb3832e25980
Nate Webber <nwebber@u.rochester.edu>
2022-11-22 00:06:30 -0500

    write neat O(n) for day 1 I found

--- days/Day1.cs
+++ days/Day1.cs
@@ -23,23 +23,44 @@ public class Day1
             }
         }
         inputList.Sort();
-        for (int i = 0; i < inputList.Count; i++)
+        for (int n = 0; n < inputList.Count; n++)
         {
-            int a = inputList[i];
+            int a = inputList[n];
             if (a > 2020)
             {
                 //admittedly we should't ever get here because we'll have missed the correct answer
                 continue;
             }
-            for (int j = i + 1; j < inputList.Count; j++)
+            for (int m = n + 1; m < inputList.Count; m++)
             {
-                int b = inputList[j];
+                int b = inputList[m];
                 if (a + b == 2020)
                 {
                     Console.WriteLine("Part 1: {0}", a * b);
-                    return;
+                    //return;
                 }
             }
         }
+        /* O(n) solution I found here 
+        https://codereview.stackexchange.com/questions/205696/determine-a-list-contains-two-elements-with-a-given-sum
+        */
+        Console.WriteLine("trying O(n)");
+        HashSet<int> theSet = new HashSet<int>();
+        int expectedSum = 2020;
+        foreach (int x in inputList)
+        {
+            if (theSet.Contains(expectedSum - x))
+            {
+                Console.WriteLine("Part 1 but O(n): {0}", x * (expectedSum - x));
+            }
+            else
+            {
+                theSet.Add(x);
+                //Console.WriteLine("set contents:");
+                //Console.WriteLine(theSet.ToString());
+            }
+        }
+        //return;
+
     }
 }
