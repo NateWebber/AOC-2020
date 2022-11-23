ec2d46214c214b8f154e4c4162af0c76e93c524c
Nate Webber <nwebber@u.rochester.edu>
2022-11-23 11:47:29 -0500

    finish Day 2

--- days/Day2.cs
+++ days/Day2.cs
@@ -36,7 +36,7 @@ public class Day2
             string? line;
             while ((line = sr.ReadLine()) != null)
             {
-                if (ProcessLine(line))
+                if (ProcessLine(line, false))
                 {
                     validCount++;
                 }
@@ -60,28 +60,35 @@ public class Day2
         else
         {
             List<int> inputList = new List<int>();
+            int validCount = 0;
             using (StreamReader sr = File.OpenText(inFilePath))
             {
                 string? line;
                 while ((line = sr.ReadLine()) != null)
                 {
-                    //Console.WriteLine(line);
+                    if (ProcessLine(line, true))
+                    {
+                        validCount++;
+                    }
                 }
             }
+            Console.WriteLine("Part 2: {0}", validCount);
         }
 
     }
 
-    private static bool ProcessLine(string line)
+    private static bool ProcessLine(string line, bool partTwo)
     {
         string[] lineSplit = line.Split(":");
         string password = lineSplit[1].Trim();
         string[] criteriaSplit = lineSplit[0].Split(" ");
         char criteriaChar = criteriaSplit[1][0];
         string[] numsSplit = criteriaSplit[0].Split("-");
-        return PasswordIsValid(password, (criteriaChar, int.Parse(numsSplit[0]), int.Parse(numsSplit[1])));
+        if (partTwo)
+            return PasswordIsValidP2(password, (criteriaChar, int.Parse(numsSplit[0]), int.Parse(numsSplit[1])));
+        return PasswordIsValidP1(password, (criteriaChar, int.Parse(numsSplit[0]), int.Parse(numsSplit[1])));
     }
-    private static bool PasswordIsValid(string password, (char, int, int) criteria)
+    private static bool PasswordIsValidP1(string password, (char, int, int) criteria)
     {
         int charCount = 0;
         foreach (char c in password)
@@ -92,6 +99,15 @@ public class Day2
             }
         }
         if (criteria.Item2 <= charCount && charCount <= criteria.Item3)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private static bool PasswordIsValidP2(string password, (char, int, int) criteria)
+    {
+        if ((password[criteria.Item2 - 1] == criteria.Item1) != (password[criteria.Item3 - 1] == criteria.Item1))
         {
             return true;
         }
